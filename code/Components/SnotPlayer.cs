210678d9758f930122cb80ebbadec2d9bcf280f3
Yaniis <yaniis.guergour@gmail.com>
2024-03-10 19:26:02 +0100

    Fixed Camera

--- code/Components/SnotPlayer.cs
+++ code/Components/SnotPlayer.cs
@@ -39,12 +39,28 @@ public sealed class SnotPlayer : Component
 	[Range( 0f, 600f, 1f )]
 	public float JumpStrength { get; set; } = 400f;
 
+	/// <summary>
+	/// Where the camero rotates around and the aim originates from
+	/// </summary>
+	[Property]
+	public Vector3 EyePosition { get; set; }
+
 	public Angles EyeAngles { get; set; }
+	Transform _initialCameraTransform;
+
+	protected override void DrawGizmos()
+	{
+		Gizmo.Draw.LineSphere( EyePosition, 10f );
+	}
 
 	protected override void OnUpdate()
 	{
 		EyeAngles += Input.AnalogLook;
+		EyeAngles = EyeAngles.WithPitch( 0f );
 		Transform.Rotation = Rotation.FromYaw( EyeAngles.yaw );
+
+		if ( Camera != null )
+			Camera.Transform.Local = _initialCameraTransform.RotateAround(EyePosition, EyeAngles.WithYaw(0f));
 	}
 
 	protected override void OnFixedUpdate()
@@ -54,12 +70,13 @@ public sealed class SnotPlayer : Component
 		if ( Controller == null ) return;
 
 		var wishSpeed = Input.Down("Run") ? RunSpeed : WalkSpeed;
-		var wishVelocity = Input.AnalogMove * wishSpeed * Transform.Rotation;
+		var wishVelocity = Input.AnalogMove.Normal * wishSpeed * Transform.Rotation;
 
 		Controller.Accelerate( wishVelocity );
 
 		if ( Controller.IsOnGround )
 		{
+			Controller.Acceleration = 10f;
 			Controller.ApplyFriction( 5f );
 
 			if ( Input.Pressed("Jump") )
@@ -72,6 +89,7 @@ public sealed class SnotPlayer : Component
 		}
 		else
 		{
+			Controller.Acceleration = 5f;
 			Controller.Velocity += Scene.PhysicsWorld.Gravity * Time.Delta;
 		}
 
@@ -86,7 +104,10 @@ public sealed class SnotPlayer : Component
 
 	protected override void OnStart()
 	{
-		base.OnStart();
+		if ( Camera != null )
+		{
+			_initialCameraTransform = Camera.Transform.Local;
+		}
 	}
 
 	protected override void OnEnabled()
